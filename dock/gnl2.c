3a696d2cfd587941cba4862dc7fa6204bdebada3
ohnudes <ohnudes@Macintosh.local>
2023-06-22 12:50:00 +0200

    Update: Prototype finished. Utils to be developed and optimised.

--- dock/gnl2.c
+++ dock/gnl2.c
@@ -6,28 +6,71 @@
 /*   By: ohnudes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 21:00:34 by ohnudes           #+#    #+#             */
-/*   Updated: 2023/06/21 22:55:25 by ohnudes          ###   ########.fr       */
+/*   Updated: 2023/06/22 12:49:51 by ohnudes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdlib.h>
+#include <unistd.h>
 
 typedef struct s_buflen
 {
 	char	*content;
+	char	*line; 
 	int		len; // EVERY MALLOC CONSIDERS +1
 
 }			t_bufdata;
 
-char	*line_assambler();
+char	*line_assambler(t_bufdata *buf, int match)
+{
+	char	*line;
+	char	*eol;
 
-char	*read_into_buff();
+	eol = buf->content;
+	line = NULL;
+	if (!eol[match])
+		line = malloc(sizeof(char) * (buf->len + 1));
+	else
+		line = malloc(sizeof(char) * (match + 1));
+	if (!line)
+		return (NULL);
+	line = ft_substr(buf->content, match); // handles '\0' termination
+	if (eol[match])
+		buf->content = ft_substr((buf->content + match), buf->len);
+	return (line);
+}
+
+int	read_into_buff(t_bufdata *buf, int fd)
+{
+	char	*tmp;
+	int		match;
+	int		rbytes;
+
+	match = 0;
+	while (!match)
+	{
+		tmp = NULL;
+		tmp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!tmp)
+			return NULL;
+		rbytes = read(fd, tmp, BUFFER_SIZE);
+		if (rbytes == -1)
+			return NULL;
+		tmp[BUFFER_SIZE] = '\0';
+		&buf = ft_strjoin(buf, tmp, rbytes); // checker comes after the strjoin.
+		match = ft_strchr(&buf); // checks buf + rbytes for reference.
+		free (tmp);
+	}
+	return (match);
+}
 
 char	*buf_checker(t_bufdata *buf)
 {
 	char	*str;
+	char	*newbuf;
 	int		i;
+	int		j;
 
 	if (!buf->content)
 		return (NULL);
@@ -41,12 +84,17 @@ char	*buf_checker(t_bufdata *buf)
 		str = malloc(sizeof(char) * (i + 1));
 		if (!str)
 			return (NULL);
+		j = i;
 		str[i--] = '\0';
-		while (i >= -1)
+		while (i >= 0)
 		{
 			str[i] = *(buf->content + i);
 			i--;
 		}
+		newbuf = malloc(sizeof(char) * (buf->len - j + 1));
+		while (j < buf->len)
+			newbuf[i++] = *(buf->content + j++);
+		newbuf[i] = '\0';
 	}
 	return (str);
 }
@@ -55,15 +103,19 @@ char	*get_next_line(int fd)
 {
 	static t_bufdata	buf; // static buffer
 	char				*line; // returned line which ends at '\n'
-	char				*ref; // first '\n' ocurrence
+	int					*match; // first '\n' ocurrence
 
 	if (fd < 0)
 		return (NULL);
+	line = NULL;
+	match = NULL;
 	line = buf_checker(&buf);
 	if (!line)
-		while (!ref)
-			ref = read_into_buff(&buf, fd);
-	line = line_assambler(ref, &buf);
+	{
+		while (!match)
+			match = read_into_buff(&buf, fd);
+		line = line_assambler(&buf, match); // line can be null
+	}
 	return (line);
 }
 
