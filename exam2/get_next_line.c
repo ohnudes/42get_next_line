3cf1c355b99da983034f938efe23131690d73f86
Nicolas Maturana Chandia <nmaturan@cbr14s1.42barcelona.com>
2023-06-26 18:00:32 +0200

    Almost there yeh

--- exam2/get_next_line.c
+++ exam2/get_next_line.c
@@ -6,36 +6,71 @@
 /*   By: nmaturan <nmaturan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 14:00:57 by nmaturan          #+#    #+#             */
-/*   Updated: 2023/06/26 16:10:51 by nmaturan         ###   ########.fr       */
+/*   Updated: 2023/06/26 17:58:04 by nmaturan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 #include <unistd.h>
 #include <stdlib.h>
 
-static char	*read_to_buffer(t_stor *buffer, int fd)
+char	*ft_free(char **buffer)
+{
+	free (*buffer);
+	*buffer = NULL;
+	return (*buffer);
+}
+
+static char	*read_to_buffer(char **buffer, int fd)
 {
 	char	*tmp;
+	int		rbytes;
+	int		match;
 
 	tmp = NULL;
-	while (!line)
-
+	tmp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!tmp)
+		return (NULL);
+	tmp[BUFFER_SIZE] = '\n';
+	match = 0;
+	while (!match || rbytes > 0)
+	{
+		rbytes = read(fd, tmp, BUFFER_SIZE);
+		if (rbytes != -1)
+		{
+			match = ft_strchr(tmp, '\n');
+			*buffer = ft_strjoin_t(*buffer, tmp);
+			if (!buffer)
+				return (NULL);
+		}
+	}
+	if (rbytes == -1)
+		return (NULL);
+	free (tmp);
+	return (*buffer);
 }
 
 char	*get_next_line(int fd)
 {
-	static char		*buffer;
+	static char		*buffer = {0};
 	char			*line;
-	size_t			
+	int				match;
+	size_t			len;
 
-	if (fd < 0 || BUFFER_SIZE < 0) 
-		return (NULL);	
-	match = ft_strchr(buffer, '\n');
-	if ((buffer && !match) || !buffer)
+	if (fd < 0 || BUFFER_SIZE < 0)
+		return (NULL);
+	if ((buffer && ft_strchr(buffer, '\n') < 0) || !buffer)
 		buffer = read_to_buffer(&buffer, fd);
 	if (!buffer)
-		return (ft_free(&buffer))
-	line = ft_substr(&buffer, 0, match);
-	buffer = ft_substr(buffer, match, len);  
+		return (ft_free(&buffer));
+	len = ft_strlen(buffer);
+	match = ft_strchr(buffer, '\n');
+	if (match < 0)
+		line = ft_substr(&buffer, 0, len);
+	else
+		line = ft_substr(&buffer, 0, match);
+	if (!line)
+		return (ft_free(&buffer));
+	buffer = ft_substr(&buffer, ft_strlen(line), len);
 	return (line);
 }
--- exam2/get_next_line_utils.c
+++ exam2/get_next_line_utils.c
@@ -6,20 +6,49 @@
 /*   By: nmaturan <nmaturan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 15:36:42 by nmaturan          #+#    #+#             */
-/*   Updated: 2023/06/26 16:16:39 by nmaturan         ###   ########.fr       */
+/*   Updated: 2023/06/26 17:42:54 by nmaturan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_substr(char *str, unsigned int start, size_t len)
+char	*ft_strjoin_t(char *s1, char *s2)
+{
+	char	*result;
+	size_t	len;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	len = ft_strlen(s1) + ft_strlen(s2);
+	result = malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	while (i < ft_strlen(s1))
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while (j < ft_strlen(s2))
+	{
+		result[i + j] = s2[j];
+		j++;
+	}
+	result[len] = '\0';
+	free (s1);
+	s1 = NULL;
+	return (result);
+}
+
+char	*ft_substr(char **str, unsigned int start, size_t len)
 {
 	char	*result;
 	size_t	i;
 
-	if (!s)
+	if (!*str)
 		return (NULL);
-	if (!len || ft_strlen(str) < (size_t)start)
+	if (!len || ft_strlen(*str) < (size_t)start)
 	{
 		result = malloc(sizeof(char) * 1);
 		if (!result)
@@ -31,27 +60,27 @@ char	*ft_substr(char *str, unsigned int start, size_t len)
 	if (!result)
 		return (ft_free(&result));
 	i = 0;
-	while (i < len && s[start + i])
+	while (i < len && *str[start + i])
 	{
-		result[i] = s[start + i];
+		result[i] = *str[start + i];
 		i++;
 	}
 	result[i] = '\0';
 	return (result);
 }
 
-size_t	ft_strchr(const char *str, char set)
+int	ft_strchr(const char *str, char set)
 {
-	char	*ref;
+	size_t	i;
 
+	i = 0;
 	if (!str)
-		return (NULL);
-	ref = str;
-	while (ref && *ref != '\n')
-		ref++;
-	if (*ref != '\n')
-		return (NULL);
-	return (ref);
+		return (-1);
+	while (str[i] && str[i] != set)
+		i++;
+	if (str[i] != set)
+		return (-1);
+	return (i);
 }
 
 size_t	ft_strlen(const char *str)
@@ -61,5 +90,5 @@ size_t	ft_strlen(const char *str)
 	i = 0;
 	while (str[i])
 		i++;
-	return (i)
+	return (i);
 }
