6cf1be56475123b48f99d644bdde111f236a3633
Nicolas Maturana Chandia <nmaturan@cbr1s5.42barcelona.com>
2023-09-17 13:35:34 +0200

    dev: bonus integration: todo: makefile integrating bonus for paco to parse.

---
+++ dock/get_next_line_bonus.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmaturan <nmaturan@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 13:31:44 by nmaturan          #+#    #+#             */
+/*   Updated: 2023/09/17 13:31:53 by nmaturan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10000000
+# endif 
+
+# include <unistd.h>
+# include <stdlib.h>
+
+// main corpus
+char	*get_next_line(int fd);
+//static char		*fill_content(int rbytes, int fd, char **eol);
+//static char		*find_eol(char	*eol, char *content, char *tmp, int rbytes);
+//static char		*produce_line(char *content, char *eol);
+//static char		*clean_content(char *content, char *eol);
+
+// utils
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, char set);
+char	*ft_strappend(char *first, char *second, int rbytes);
+char	*ft_free(char *str);
+
+#endif
